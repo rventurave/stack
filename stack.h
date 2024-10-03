f203b7cd2178ccb1c6f2ace50529dd6fbf614517
rventurave <rventurave@unsa.edu.pe>
2024-10-03 11:42:58 -0500

    Corrigiendo la identacion y las variablesde SIZE

--- stack.h
+++ stack.h
@@ -1,10 +1,10 @@
 #ifndef STACK_H
 #define STACK_H
-#define SIZE 100;
-typedef struct Stack{
-int data[SIZE];
-int top;
-}Stack;
+#define SIZE 100
+typedef struct Stack {
+  int data[SIZE];
+  int top;
+} Stack;
 void initStack(Stack *s);
 void push(Stack *s, int data);
 void pop(Stack *s);
